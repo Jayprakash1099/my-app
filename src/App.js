dd5b6d7d2193b327c38e87d8d507f73fbac42b6f
Jayprakash1099 <109289652+Jayprakash1099@users.noreply.github.com>
2024-02-23 23:11:16 +0530

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black ">
+      <div className='w-1/2 m-auto bg-red-400 rounded-full'>
       <Hema/>
       <Jaya/>
+      </div>
 
     </div>
   );
--- src/component/hemant.js
+++ src/component/hemant.js
@@ -1,7 +1,7 @@
 function Hema() {
     return (
       <div className="App">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj3qopxOBnULdJ1GjsVRhdcCyLLhSAxZrpfFnPcBKJIg&s" className="App-logo" alt="logo" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj3qopxOBnULdJ1GjsVRhdcCyLLhSAxZrpfFnPcBKJIg&s" className="App-logo h-4  " alt="logo" />
           <h1>this is hema loves hemant.</h1>
           
       </div>
