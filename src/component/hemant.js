33b071a49cba727abb9add9bb9484079a06dc83c
Jayprakash1099 <109289652+Jayprakash1099@users.noreply.github.com>
2024-02-23 22:55:46 +0530

    added hemsnt smf Jsy drction
    hema love shemnat and jaya love sjya

---
+++ src/component/hemant.js
@@ -0,0 +1,12 @@
+function Hema() {
+    return (
+      <div className="App">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj3qopxOBnULdJ1GjsVRhdcCyLLhSAxZrpfFnPcBKJIg&s" className="App-logo" alt="logo" />
+          <h1>this is hema loves hemant.</h1>
+          
+      </div>
+    );
+  }
+  
+  export default Hema;
+  
---
+++ src/component/jay.js
@@ -0,0 +1,12 @@
+function Jaya() {
+    return (
+      <div className="App">
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScKXniL1QPVkWjtF48QJR-WJdbHQ6EU5X3Pi25qJ5gHA&s' className="App-logo" alt="logo" />
+          <h1>jay loves everyone.</h1>
+          
+      </div>
+    );
+  }
+  
+  export default Jaya;
+  
